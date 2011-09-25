b795d54de1ee62de1453679f82aef040d83ca0f8
Eric Park <epark@jeztek.com>
2011-09-24 17:11:50 -0700

    nearing final 1st rev

--- app/views.py
+++ app/views.py
@@ -18,26 +18,16 @@ from helpers import stringToEpochMilli
 from account.forms import Timezone as TimezoneForm
 
 
-# Return JSON encoded representation of object as text response
 dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.time) else None
+
+# Return JSON encoded representation of object as text response
 def JsonResponse(obj):
     return HttpResponse(json.dumps(obj, ensure_ascii=False, default=dthandler),
                         mimetype="text/plain; charset=\"utf-8\"")
 
-def about(request):
-    return HttpResponseRedirect('/')
-
-def blog(request):
-    return HttpResponseRedirect('/')
-
-
-def contact(request):
-    return HttpResponseRedirect('/')
-
     
 @login_required
 def home(request, message=None):
-    print message
     now = int(time.time()*1000)
     timezone = pytz.timezone(request.user.get_profile().tz)
 
@@ -49,7 +39,7 @@ def flatten(listOfLists):
     "Flatten one level of nesting"
     return chain.from_iterable(listOfLists)
 
-@login_required                              
+@login_required
 def meds(request):
 
     user_profile = request.user.get_profile()
