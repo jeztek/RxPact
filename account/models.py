a365ecaebe598c43fc27f6cb1b5c06f032597d86
Alex Khomenko <khomenko@cs.stanford.edu>
2011-09-24 20:33:28 -0700

    add picture_name to medication

--- account/models.py
+++ account/models.py
@@ -14,6 +14,7 @@ class UserProfile(models.Model):
 
 class Medication(models.Model):
     name = models.CharField(max_length=50)
+    picture_name = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.name
